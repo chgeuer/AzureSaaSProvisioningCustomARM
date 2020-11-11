bd19a5b633197a89cc9eb0c6d752553cd3a7cb30
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-11-11 16:03:23 +0100

    a

--- LandingPage/SampleParametrization.cs
+++ LandingPage/SampleParametrization.cs
@@ -7,17 +7,21 @@
     {
         public string SomeClientInformation { get; set; }
 
-        public IContentPatcher CreatePatcher () => new GenericJSONPatcher(patchARM: patchARM, patchUI: patchUI);
-
-        void patchARM(JObject json)
+        public IContentPatcher CreatePatcher() 
         {
-            var variables = json.SelectToken("$.variables");
-            variables["dynamically_injected"] = new JObject(new JProperty("some_client_stuff", this.SomeClientInformation));
-        }
+            void patchARM(JObject json)
+            {
+                var variables = json.SelectToken("$.variables");
+ 
+                variables["dynamically_injected"] = new JObject(new JProperty("some_client_stuff", this.SomeClientInformation));
+            }
 
-        void patchUI(JObject json)
-        {
-            json["$greetings"] = this.SomeClientInformation;
+            void patchUI(JObject json)
+            {
+                json["$greetings"] = this.SomeClientInformation;
+            }
+
+            return new GenericJSONPatcher(patchARM: patchARM, patchUI: patchUI);
         }
     }
 }
